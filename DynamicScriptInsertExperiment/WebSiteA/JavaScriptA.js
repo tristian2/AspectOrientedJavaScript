da929163f30f4d36ebaf3430165cdb5b1a793b9a
tristian o'brien <tristian2@hotmail.com>
2015-04-08 11:47:15 +0000

    this code demonstrates Dynamic script insertion.
    important for several reasons
    * could describe a rudimentary mechanism for adding Advice
    * how to add advice in these scenarios
    
    why important?
    * well this is a technique used by developers to get around cross domain issues.  so if you supply a platform that has a JS elemetn such as piwik analytics, you'd describe to developers how to use so that they can pass data into the application - a API for cross domain scenarios

---
+++ DynamicScriptInsertExperiment/WebSiteA/JavaScriptA.js
@@ -0,0 +1,12 @@
+﻿var a = 43 || [];
+console.log("in javascript A on Website A");
+$( document ).ready(function() {
+    $("#result").text("in javascript A on Website A");
+});
+
+(function () {
+    var u = "//localhost:55976/javascriptb.js";
+    var b = "hello";
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u; s.parentNode.insertBefore(g, s);
+})();
---
+++ DynamicScriptInsertExperiment/WebSiteB/JavaScriptB.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $("#result").append("<br/>in websiteB, javascript B");
+    $("#result").append("<br/>value of a" + a);
+    $("#result").append("<br/>new value of a" + eval(a + a));
+    $("#result").append("<br/>value of b: " + b);
+});
+
